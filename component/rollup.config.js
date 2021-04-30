0f7b693ccee1ff5770366c29d39712c2feb52642
Kevin Orriss <kevinorriss@gmail.com>
2021-04-30 22:24:00 +0100

    v1.0.1

--- component/rollup.config.js
+++ component/rollup.config.js
@@ -1,25 +1,28 @@
-import babel from 'rollup-plugin-babel'
+import babel from '@rollup/plugin-babel'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs';
 import { uglify } from 'rollup-plugin-uglify'
 import postcss from 'rollup-plugin-postcss'
+import { terser } from 'rollup-plugin-terser'
 
 const config = {
     input: 'src/Calculator.js',
-    external: ['react'],
     plugins: [
         postcss({
-            extensions: [ '.css' ]
+            extensions: [ '.css' ],
         }),
         babel({
-            exclude: "node_modules/**"
+            exclude: "node_modules/**",
+            babelHelpers: 'bundled'
         }),
+        nodeResolve(),
+        commonjs(),
+        terser(),
         uglify()
     ],
     output: {
         format: 'umd',
-        name: 'bundle',
-        globals: {
-            react: "React"
-        }
+        name: 'bundle'
     }
 }
 export default config
--- src/index.js
+++ src/index.js
@@ -2,4 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Calculator from '@kevinorriss/calculator'
 
-ReactDOM.render(<Calculator/>, document.getElementById('root'))
+ReactDOM.render(
+    <div style={{ width: '40rem', textAlign:'center'}}>
+        <p>Header</p>
+        <div style={{ display:'flex', flexDirection:'row', justifyContent:'center', alignItems:'center' }}>
+            <p>Left</p>
+            <div style={{ flexGrow:1 }}>
+                <Calculator />
+            </div>
+            <p>Right</p>
+        </div>
+        <p>Footer</p>
+    </div>
+, document.getElementById('root'))
